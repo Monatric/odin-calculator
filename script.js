cb6b6b1436de7ffef492a272965cd489b7a2d4d3
Monatric <nawpnawp8@gmail.com>
2024-03-10 17:38:24 +0800

    Create global variables for first number, second number, and operator, and function that processes them

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
 function add(operand1, operand2) {
     return operand1 + operand2;
 }
@@ -14,7 +18,20 @@ function divide(operand1, operand2) {
     return operand1 / operand2;
 }
 
-console.log(add(2, 1))
-console.log(subtract(1, 2))
-console.log(multiply(2, 1))
-console.log(divide(2, 8))
+function operate(firstNum, operator, secondNum) {
+    if (operator === '+') {
+        return add(firstNum, secondNum)
+    } else if (operator === '-') {
+        return subtract(firstNum, secondNum)
+    } else if (operator === '*') {
+        return multiply(firstNum, secondNum)
+    } else if (operator === '/') {
+        return divide(firstNum, secondNum)
+    }
+}
+
+// console.log(add(2, 1))
+// console.log(subtract(1, 2))
+// console.log(multiply(2, 1))
+// console.log(divide(2, 8))
+console.log(operate(3, '*', 5))
