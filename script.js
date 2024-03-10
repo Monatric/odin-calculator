888947003e5e3ae163eabd147ffb8ded9e30766f
Monatric <nawpnawp8@gmail.com>
2024-03-10 17:22:47 +0800

    Add 4 math basic operator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(operand1, operand2) {
+    return operand1 + operand2;
+}
+
+function subtract(operand1, operand2) {
+    return operand1 - operand2;
+}
+
+function multiply(operand1, operand2) {
+    return operand1 * operand2;
+}
+
+function divide(operand1, operand2) {
+    return operand1 / operand2;
+}
+
+console.log(add(2, 1))
+console.log(subtract(1, 2))
+console.log(multiply(2, 1))
+console.log(divide(2, 8))
