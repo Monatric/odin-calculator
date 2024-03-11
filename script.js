4e8ec56a070c8e8141589164044a8063e7ccc2a7
Monatric <nawpnawp8@gmail.com>
2024-03-11 20:53:15 +0800

    Buttons, like numbers and operators populate the display

--- script.js
+++ script.js
@@ -38,36 +38,127 @@ function operate(firstNum, operator, secondNum) {
 // attempt 1
 
 // not sure if this works so test
-let displayValue = 0;
 // let operatorValue = '';
 
-console.log(calcBtnsEqu)
 // let calcBtnsContent = calcBtns.textContent;
 
 
 //different attempt 2
-let calcBtns = document.querySelector('');
-let calcBtnsContent = calcBtns.textContent;
+let btnRows = document.querySelectorAll('.btn-row');
 let display = document.querySelector('.display');
+let displayValue = 0;
+
+btnRows.forEach((row) => {
+    row.addEventListener('click', (event) => {
+        let target = event.target;
+
+        switch (target.id) {
+            // 0-9
+            case 'zer':
+                display.textContent += '0';
+                console.log('this is zero')
+                break;
+            case 'one':
+                display.textContent += '1';
+                console.log('this is one')
+                break;
+            case 'two':
+                display.textContent += '2';
+                console.log('this is two')
+                break;
+            case 'thr':
+                display.textContent += '3';
+                console.log('this is three')
+                break;
+            case 'fou':
+                display.textContent += '4';
+                console.log('this is four')
+                break;
+            case 'fiv':
+                display.textContent += '5';
+                console.log('this is five')
+                break;
+            case 'six':
+                display.textContent += '6';
+                console.log('this is six')
+                break;
+            case 'sev':
+                display.textContent += '7';
+                console.log('this is seven')
+                break;
+            case 'eig':
+                display.textContent += '8';
+                console.log('this is eight')
+                break;
+            case 'nin':
+                display.textContent += '9';
+                console.log('this is nine')
+                break;
+            
+            // operators
+            case 'plu':
+                display.textContent += '+';
+                console.log('this is nine')
+                break;
+            case 'min':
+                display.textContent += '-';
+                console.log('this is nine')
+                break;
+            case 'mul':
+                display.textContent += '*';
+                console.log('this is nine')
+                break;
+            case 'div':
+                display.textContent += '/';
+                console.log('this is nine')
+                break;
+            case 'equ':
+                display.textContent += '=';
+                console.log('this is nine')
+                break;
+            case 'cle':
+                display.textContent = '';
+                break;
+        }
+    })
+})
+
+// btnRows.addEventListener('click', (event) => {
+//     let target = event.target;
+
+//     switch (target.id) {
+//         case 'sev':
+//             console.log('this is seven')
+//             break;
+//         case 'eig':
+//             console.log('this is eight')
+//             break;
+//         case 'fou':
+//             console.log('this is four')
+//     }
+// })
+
+console.log(btnRows)
+// let calcBtnsContent = calcBtns.textContent;
 // if (calcBtns === '7') {
 //     console.log('works')
 // }
 
-calcBtns.addEventListener('click', () => {
+// calcBtns.addEventListener('click', () => {
     
-    switch (calcBtnsContent) {
-        case '7':
-            display.textContent += '7';
-            console.log('7')
-            break;
-        case '8':
-            display.textContent = '8';
-            console.log('8')
-            break;
-    }
-})
+//     switch (calcBtnsContent) {
+//         case '7':
+//             display.textContent += '7';
+//             console.log('7')
+//             break;
+//         case '8':
+//             display.textContent = '8';
+//             console.log('8')
+//             break;
+//     }
+// })
 
-console.log(calcBtns)
+// console.log(calcBtns)
 
 
 console.log(operate(3, '*', 5))
